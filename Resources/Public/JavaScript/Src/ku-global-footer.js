ff71334eb2f2d200b0ae4fee0c8edc716c623886
nanna-dk <nanna@e-nanna.dk>
2021-12-03 10:26:28 +0100

    Init

---
+++ Resources/Public/JavaScript/Src/ku-global-footer.js
@@ -0,0 +1,78 @@
+/* ========================================================================
+ * Copyright 2021
+ * University of Copenhagen, FA Communications
+ * ========================================================================*/
+
+document.addEventListener('DOMContentLoaded', () => {
+  'use strict';
+
+  const footerHeading = document.querySelectorAll('.footer-section-content .frame-header');
+
+  const isMobile = () => {
+    return window.matchMedia('(max-width: 767px)').matches;
+  };
+
+  const setAriaAttr = () => {
+    // Set accessible attributes
+    footerHeading.forEach(el => {
+      el.setAttribute('aria-expanded', isMobile() ? 'false' : 'true');
+    });
+  };
+
+  const resetFooter = () => {
+    // Reset styling
+    footerHeading.forEach(el => {
+      el.nextElementSibling.style.removeProperty('height');
+      el.nextElementSibling.classList.remove('active');
+    });
+  };
+
+  function toggleFooter() {
+    footerHeading.forEach(el => {
+      el.addEventListener('click', (e) => {
+        let that = e.currentTarget;
+        let list = that.nextElementSibling;
+
+        // Slide down
+        if (!list.classList.contains('active')) {
+          list.classList.add('active');
+          list.style.height = 'auto';
+
+          let height = list.clientHeight + 'px';
+          list.style.height = '0';
+          setTimeout(() => {
+            list.style.height = height;
+          }, 0);
+          that.setAttribute('aria-expanded', 'true');
+
+        } else {
+          // Slide up
+          list.style.height = '0';
+          that.setAttribute('aria-expanded', 'false');
+
+          // Remove the `active` class when the animation ends
+          list.addEventListener('transitionend', () => {
+            list.classList.remove('active');
+          }, {
+            once: true
+          });
+        }
+      });
+    });
+
+  }
+
+  setAriaAttr();
+  toggleFooter();
+
+  window.addEventListener('resize', debounce(() => {
+    resetFooter();
+    setAriaAttr();
+  }, 150));
+
+  window.addEventListener('orientationchange', debounce(() => {
+    resetFooter();
+    setAriaAttr();
+  }, 150));
+
+});
