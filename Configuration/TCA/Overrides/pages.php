3448eba56a9bdae7f846b4a5f7e4f3db72bb4bc3
nanna-dk <nanna@e-nanna.dk>
2022-05-10 10:34:23 +0200

    Faculty dropdoown

--- Configuration/TCA/Overrides/pages.php
+++ Configuration/TCA/Overrides/pages.php
@@ -1,7 +1,43 @@
 <?php
+
 defined('TYPO3_MODE') || die();
-call_user_func(function()
-{
+
+// Add some fields to fe_users table to show TCA fields definitions
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns(
+    'pages',
+    [
+        'ku_faculty' => [
+            'exclude' => 0,
+            'label' => 'Vælg fakultet',
+            'description' => '',
+            'config' => [
+                'type' => 'select',
+                'renderType' => 'selectSingle',
+                'items' => [
+                    ['Vælg fakultet', '--div--'],
+                    ['KU', 1],
+                    ['Det Humanistiske Fakultet', 2],
+                    ['Det Sundhendvidenskabelige Fakultet', 3],
+                    ['Det Juridiske Fakultet', 4],
+                    ['Det Teologiske Fakultet', 5],
+                    ['Det Natur- og Biovidenkabelige Fakultet', 6],
+                    ['Det Samfundsvidenskabelige Fakultet', 7],
+                ],
+                'size' => 1,
+                'maxitems' => 1,
+            ],
+        ],
+    ]
+);
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes(
+    'pages',
+    'ku_faculty',
+    '1,3,4',
+    'before:abstract'
+);
+
+call_user_func(function () {
     /**
      * Temporary variables
      */
