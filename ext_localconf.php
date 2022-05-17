316034024d60d9c942dd56ab2294eebfdd4b0e95
nanna-dk <nanna@e-nanna.dk>
2022-05-17 10:18:50 +0200

    Added comment

--- ext_localconf.php
+++ ext_localconf.php
@@ -21,7 +21,7 @@ $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['ku_news'] = 'EXT:ku_prototype/Con
 // KU Viewhelper namespace
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['fluid']['namespaces']['ku'] = ['UniversityOfCopenhagen\KuPrototype\ViewHelpers'];
 
-// Add custom database field to rootline
+// Add custom database field to rootline - keep the '&'
 $rootlinefields = &$GLOBALS["TYPO3_CONF_VARS"]["FE"]["addRootLineFields"];
 if($rootlinefields != '')
 {
