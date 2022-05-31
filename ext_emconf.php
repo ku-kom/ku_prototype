6619abe8cafc6e01e10ed3d67c158aecb86e2689
nanna-dk <nanna@e-nanna.dk>
2022-05-31 12:34:01 +0200

    Minor fixes

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,8 +1,11 @@
 <?php
 
-/**
- * Extension Manager/Repository config file for ext "ku_prototype".
+/*
+ * This file is part of the package ku_prototype.
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
  */
+
 $EM_CONF[$_EXTKEY] = [
     'title' => 'KU prototype',
     'description' => 'KU demo design.',
