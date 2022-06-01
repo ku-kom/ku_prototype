f815e376e3fd0220c294496b56838646b4ccf82b
nanna-dk <nanna@e-nanna.dk>
2022-06-01 14:02:55 +0200

    Recreate left menu with typoscript

--- ext_emconf.php
+++ ext_emconf.php
@@ -29,5 +29,5 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'Nanna Ellegaard',
     'author_email' => 'nel@adm.ku.dk',
     'author_company' => 'University of Copenhagen',
-    'version' => '2.0.14',
+    'version' => '2.0.15',
 ];
