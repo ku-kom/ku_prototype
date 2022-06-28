5b7d4810e03fc5b31e546324f04962c9b95e6ad9
nanna-dk <nanna@e-nanna.dk>
2022-06-28 15:12:11 +0200

    Userfunc

---
+++ Classes/UserFunctions/Time.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the package ku_prototype.
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+namespace UniversityOfCopenhagen\KuPrototype\UserFunctions;
+
+final class Time
+{
+    /**
+     * Output the current time
+     *
+     * @param  string          Empty string (no content to process)
+     * @param  array           TypoScript configuration
+     * @return string          HTML output, showing the current server time.
+     */
+    public function printTime(string $content, array $conf): string
+    {
+        date_default_timezone_set('Europe/Copenhagen');
+        return '<div class="current-time">' . date('H:i:s') . '</div>';
+    }
+}
