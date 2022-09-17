a6174c3460cd1e9174b19463b77bd6bf34789397
nanna-dk <nanna@e-nanna.dk>
2022-09-17 18:12:40 +0200

    Indent

--- Classes/EventListener/FileUploadEvent.php
+++ Classes/EventListener/FileUploadEvent.php
@@ -6,6 +6,7 @@ declare(strict_types=1);
  * This file is part of the package ku_prototype.
  * For the full copyright and license information, please read the
  * LICENSE file that was distributed with this source code.
+ * Sep 2022 Nanna Ellegaard, University of Copenhagen.
  */
 
 namespace UniversityOfCopenhagen\KuPrototype\EventListener;
