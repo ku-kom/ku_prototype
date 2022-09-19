bae955f2492419e374277e4d0c85f509d296e2fb
nanna-dk <nanna@e-nanna.dk>
2022-09-19 15:24:10 +0200

    Comment

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -10,3 +10,7 @@ defined('TYPO3') or die('Access denied.');
 
 // Container extension - rename Content Element Wizard label - also check TCEMAIN
 $GLOBALS['TCA']['tt_content']['columns']['CType']['config']['itemGroups']['container'] = 'LLL:EXT:ku_prototype/Resources/Private/Language/locallang_be.xlf:container-grid';
+
+
+// Make image alternative text mandatory:
+// $GLOBALS['TCA']['sys_file_reference']['columns']['alternative']['config']['eval'] = 'required';
