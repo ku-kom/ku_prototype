91140b418c6d3be2b172f6254b5c4787d43e59b0
nanna-dk <nanna@e-nanna.dk>
2022-09-22 12:33:28 +0200

    Make image upload mandatory for Ctype "Image"

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -4,6 +4,7 @@
  * This file is part of the package ku_prototype.
  * For the full copyright and license information, please read the
  * LICENSE file that was distributed with this source code.
+ * Sep 2022 Nanna Ellegaard, University of Copenhagen.
  */
 
 defined('TYPO3') or die('Access denied.');
@@ -11,6 +12,12 @@ defined('TYPO3') or die('Access denied.');
 // Container extension - rename Content Element Wizard label - also check TCEMAIN
 $GLOBALS['TCA']['tt_content']['columns']['CType']['config']['itemGroups']['container'] = 'LLL:EXT:ku_prototype/Resources/Private/Language/locallang_be.xlf:container-grid';
 
+// Make image upload mandatory in Ctype "Image only"
+$GLOBALS['TCA']['tt_content']['types']['image']['columnsOverrides']['image']['config'] = [
+    'minitems' => 1
+];
 
 // Make image alternative text mandatory:
-// $GLOBALS['TCA']['sys_file_reference']['columns']['alternative']['config']['eval'] = 'required';
+// if (isset($GLOBALS['TCA']['sys_file_reference']['columns']['alternative']['config']['eval'])) {
+//     $GLOBALS['TCA']['sys_file_reference']['columns']['alternative']['config']['eval']  = 'required';
+// }
