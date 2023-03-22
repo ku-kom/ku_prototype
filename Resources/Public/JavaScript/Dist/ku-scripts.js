f7c29d7c083dd426332e3a3a4052f2a840a57385
Nanna Ellegaard <nel@adm.ku.dk>
2023-03-22 15:22:23 +0100

    Refactoring

--- Resources/Public/JavaScript/Dist/ku-scripts.js
+++ Resources/Public/JavaScript/Dist/ku-scripts.js
@@ -3,91 +3,120 @@
  * University of Copenhagen, FA Communications
  * ========================================================================*/
 
+/* ========================================================================
+ * Copyright 2022 Nanna Ellegaard
+ * University of Copenhagen, FA Communications
+ * ========================================================================*/
+
 document.addEventListener('DOMContentLoaded', () => {
     'use strict';
 
-    const footerHeading = document.querySelectorAll('.footer-section-content .frame-header');
-
-    if (!footerHeading) {
-        /**
-         * Break if element is not found
-         */
+    /**
+     * Check if global debounce() exists.
+     */
+    if (typeof debounce !== 'function') {
         return;
     }
 
-    const isMobile = () => {
-        return window.matchMedia('(max-width: 767px)').matches;
-    };
-
     /**
-     * Set accessible attributes
+     * Footer column elements.
      */
-    const setAriaAttr = () => {
-        footerHeading.forEach(el => {
-            el.setAttribute('aria-expanded', isMobile() ? 'false' : 'true');
-        });
-    };
+    const footerHeading = document.querySelectorAll('.footer-section-content .frame-header');
 
-    /**
-     * Reset styling
-     */
-    const resetFooter = () => {
-        footerHeading.forEach(el => {
-            el.nextElementSibling.style.removeProperty('height');
-            el.nextElementSibling.classList.remove('active');
-        });
-    };
-
-
-    const toggleFooter = () => {
-        footerHeading.forEach(el => {
-            el.addEventListener('click', (e) => {
-                let that = e.currentTarget;
-                let list = that.nextElementSibling;
-
-                if (!list.classList.contains('active')) {
-                    /**
-                     * Slide down
-                     */
-                    list.classList.add('active');
-                    list.style.height = 'auto';
-
-                    let height = list.clientHeight + 'px';
-                    list.style.height = '0';
-                    setTimeout(() => {
-                        list.style.height = height;
-                    }, 0);
-                    that.setAttribute('aria-expanded', 'true');
-
-                } else {
-                    /**
-                     * Slide up
-                     */
-                    list.style.height = '0';
-                    that.setAttribute('aria-expanded', 'false');
-
-                    /**
-                     * Remove the `active` class when the animation ends
-                     */
-                    list.addEventListener('transitionend', () => {
-                        list.classList.remove('active');
-                    }, {
-                        once: true
-                    });
-                }
-            });
-        });
+    if (!footerHeading) {
+        return;
     }
 
-    setAriaAttr();
-    toggleFooter();
+    class Footer {
+        constructor(footer) {
+            this.footer = footer;
+            this.list = this.footer.nextElementSibling;
+            this.setAriaAttr();
+            this.addEventListeners();
+        }
 
-    ['orientationchange', 'resize'].forEach(debounce((e) => {
-        window.addEventListener(e, resetFooter);
-        window.addEventListener(e, setAriaAttr);
-    }, 150));
+        /**
+         * Detect screen size.
+         * @returns bool.
+         */
+        isMobile() {
+            return window.matchMedia('(max-width: 767px)').matches;
+        }
+
+        /**
+         * Add attributes.
+         */
+        setAriaAttr() {
+            this.footer.setAttribute('aria-expanded', this.isMobile() ? 'false' : 'true');
+        }
+
+        /**
+         * Clear footer.
+         */
+        resetFooter() {
+            this.list.style.removeProperty('height');
+            this.list.classList.remove('active');
+        }
+
+        /**
+         * Slide footer columns open or closed.
+         */
+        toggleFooter() {
+            if (!this.list.classList.contains('active')) {
+                /**
+                 * Slide down
+                 */
+                this.list.classList.add('active');
+                this.list.style.height = 'auto';
+
+                let height = this.list.clientHeight + 'px';
+                this.list.style.height = '0';
+                setTimeout(() => {
+                    this.list.style.height = height;
+                }, 0);
+                this.footer.setAttribute('aria-expanded', 'true');
+
+            } else {
+                /**
+                 * Slide up
+                 */
+                this.list.style.height = '0';
+                this.footer.setAttribute('aria-expanded', 'false');
+
+                /**
+                 * Remove the `active` class when the animation ends
+                 */
+                this.list.addEventListener('transitionend', () => {
+                    this.list.classList.remove('active');
+                }, {
+                    once: true
+                });
+            }
+        }
+
+        addEventListeners() {
+            this.footer.addEventListener('click', () => {
+                this.toggleFooter();
+            });
+            ['orientationchange', 'resize'].forEach(debounce((e) => {
+                /**
+                 * Debounce functions.
+                 */
+                window.addEventListener(e, this.resetFooter());
+                window.addEventListener(e, this.setAriaAttr());
+            }, 150));
+        }
+    }
+
+    /**
+     * Assign class to footer headings.
+     */
+    footerHeading.forEach((column) => {
+        const footerEl = new Footer(column);
+    });
 
 });
+
 /* eslint-disable no-redeclare */
 /* ========================================================================
  * Copyright 2022
